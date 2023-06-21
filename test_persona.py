611759aced4bd4d79e65e348de4027352613bd19
ernie.contreras <ernie.acf@gmail.com>
2023-06-21 14:37:53 -0600

    validar RFC

--- test_persona.py
+++ test_persona.py
@@ -29,3 +29,33 @@ class TestPersona:
 		persona = Persona(nombre="María Alejandra", edad=22)
 		assert persona.calcular_anio_nacimiento(True) == datetime.datetime.now().year - 22
 		assert persona.calcular_anio_nacimiento(False) == datetime.datetime.now().year - 22 + 1
+
+	def test_rfc(self):
+		casos_prueba = {
+			"RFC valido": {
+				"rfc": "GODJ980101H31",
+				"resultado": True
+			},
+			"RFC invalido": {
+				"rfc": "GODJ980101H3",
+				"resultado": False
+			},
+			"RFC vacio": {
+				"rfc": "",
+				"resultado": False
+			},
+			"RFC con más carácteres": {
+				"rfc": "GODJ980101H312",
+				"resultado": False
+			},
+		}
+
+		for caso in casos_prueba:
+			resultado_esperado = casos_prueba[caso]["resultado"]
+			rfc = casos_prueba[caso]["rfc"]
+			persona = Persona(nombre="María Alejandra", edad=22)
+
+			persona.asignar_rfc(rfc)
+			print("Caso de prueba: " + caso)
+
+			assert persona.validar_rfc() == resultado_esperado
